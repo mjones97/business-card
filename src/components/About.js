45a3179977e1e4d323e2a8cf177d7f02285ec54b
Malik <97180802+mjones97@users.noreply.github.com>
2022-06-09 20:14:13 -0400

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -6,10 +6,9 @@ export default function About() {
     <main className={styles.about}>
       <h2 className={styles.header}>About</h2>
       <p className={styles.texts}>
-        I am a frontend developer who loves building things. Helping people to
-        have a great experience while navigating a website is a must. Providing
-        accessibility is important, also one of my concerns is to create
-        equality for everybody.
+        I am a frontend developer who loves solving problems. Helping people 
+        have a great experience while navigating a website is a must. I code to
+        express my creativity with no limits. 
       </p>
     </main>
   );
